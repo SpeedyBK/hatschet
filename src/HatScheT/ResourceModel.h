1a840f03960ef409cc8df05a7c3b7c8996d4a496
Julian Oppermann <oppermann@esa.informatik.tu-darmstadt.de>
2018-01-21 21:56:30 +0100

    Early draft of the resource model.

--- src/HatScheT/ResourceModel.h
+++ src/HatScheT/ResourceModel.h
@@ -4,9 +4,60 @@
 #include <HatScheT/Vertex.h>
 #include <HatScheT/Edge.h>
 #include <map>
+#include <set>
+#include <list>
+#include <memory>
 
 namespace HatScheT
 {
+
+struct Resource
+{
+  const int id;
+  const std::string name;
+  const int availableUnits;
+
+  Resource(int id, std::string name, int availableUnits);
+};
+
+typedef std::shared_ptr<Resource> ResourceRef;
+
+struct BlockReservation
+{
+  const int start, duration;
+};
+
+class ReservationTable
+{
+public:
+  // builder interface
+  void addReservation(ResourceRef resource);
+  void addReservation(ResourceRef resource, int start);
+  void addReservation(ResourceRef resource, int start, int duration);
+
+  // query interface
+  //  1) "raw" access to the table
+  std::set<ResourceRef>       getResources() const;
+  std::list<BlockReservation> getReservations(ResourceRef resource) const;
+  //  2) query a specific timestep
+  bool                        usesResourceAt(ResourceRef resource, int timestep) const;
+  int                         getResourceDemandAt(ResourceRef resource, int timestep) const;
+  //     ... do we need a "give me all resources (and their demand) at t" method?
+  //  3) summary view
+  int                         getMaxResourceDemand(ResourceRef resource) const;
+
+
+  bool isSimple() const;
+  bool isBlock() const;
+  bool isComplex() const;
+
+  void dump() const;
+
+private:
+  std::map<ResourceRef, std::list<BlockReservation>> table;
+};
+
+typedef std::shared_ptr<ReservationTable> ReservationTableRef;
 
 /*!
  * \brief ResourceModel
@@ -16,9 +67,24 @@ class ResourceModel
 public:
   ResourceModel(Graph &g);
 
+  // factories for Resources
+  ResourceRef makeResource(std::string name);
+  ResourceRef makeResource(std::string name, int available);
 
+  // factories for ReservationTables
+  ReservationTableRef getEmptyReservationTable();
+  ReservationTableRef getSimpleReservationTable(ResourceRef resource);
+  ReservationTableRef getBlockReservationTable(ResourceRef resource, int duration);
 
-protected:
+  // association of vertices/reservation tables
+  void registerVertex(Vertex v, ReservationTableRef reservationTable);
+  ReservationTableRef getReservationTable(Vertex v);
+
+private:
   Graph &g;
+
+  // TODO: we want to encapsulate the resource handling from the vertices, right?
+  // TODO: by value?
+  std::map<Vertex, ReservationTableRef> resourceMap;
 };
 }
