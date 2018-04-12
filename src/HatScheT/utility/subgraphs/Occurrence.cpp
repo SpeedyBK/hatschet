bd5e9bf591bdd88c324942af8a771d1b52310567
Patrick Sittel <sittel@uni-kassel.de>
2018-04-12 17:28:00 +0200

    added occurrence functions

--- src/HatScheT/utility/subgraphs/Occurrence.cpp
+++ src/HatScheT/utility/subgraphs/Occurrence.cpp
@@ -2,8 +2,49 @@
 
 namespace HatScheT
 {
-Occurrence::Occurrence()
+Occurrence::Occurrence(Graph *g)
 {
-
+  this->g =g;
 }
+
+bool Occurrence::addEdge(Edge *e)
+{
+  if(this->edges.size()==0){
+    this->edges.insert(e);
+    this->vertices.insert(&e->getVertexDst());
+    this->vertices.insert(&e->getVertexSrc());
+    return true;
+  }
+
+  const bool is_in = this->edges.find(e) != this->edges.end();
+
+  if(is_in==true){
+    cout << "Occurrence.addEdge: WARNING tried to add an existing edge" << endl;
+    return false;
+  }
+
+  if(this->isConnected(e)==true){
+    this->edges.insert(e);
+    this->vertices.insert(&e->getVertexDst());
+    this->vertices.insert(&e->getVertexSrc());
+    return true;
+  }
+  else{
+    cout << "Occurrence.addEdge: WARNING tried to add a non connected edge" << endl;
+    return false;
+  }
+}
+
+bool Occurrence::isConnected(Edge *e)
+{
+  for(auto it=this->edges.begin();it!=this->edges.end();++it){
+    Edge* iterE = *it;
+    if(&e->getVertexDst()==&iterE->getVertexDst()) return true;
+    if(&e->getVertexSrc()==&iterE->getVertexDst()) return true;
+    if(&e->getVertexDst()==&iterE->getVertexSrc()) return true;
+    if(&e->getVertexSrc()==&iterE->getVertexSrc()) return true;
+  }
+  return false;
+}
+
 }
--- src/HatScheT/utility/subgraphs/Occurrence.h
+++ src/HatScheT/utility/subgraphs/Occurrence.h
@@ -5,20 +5,58 @@
 */
 #pragma once
 
+#include <HatScheT/Graph.h>
+
 namespace HatScheT
 {
-
+/*!
+ * \brief The Occurrence class This class can be used for describing occurrences of subgraphs in the Graph g
+ */
 class Occurrence
 {
 public:
-
-  Occurrence();
-
-
+  /*!
+   * \brief Occurrence
+   * \param g
+   */
+  Occurrence(Graph *g);
+  /*!
+   * \brief addEdge try to add an edge to the occurrence. It has to be a new and connected Edge.
+   * \param e
+   * \return
+   */
+  bool addEdge(Edge* e);
+  /*!
+   * \brief getEdges
+   * \return
+   */
+  std::set<Edge*> getEdges(){return this->edges;}
+  /*!
+   * \brief getVertices
+   * \return
+   */
+  std::set<Vertex*> getVertices(){return this->vertices;}
 protected:
 
 private:
-
+  /*!
+   * \brief isConnected determine whether the edge is connected to the already existing edges in g
+   * \param e
+   * \return
+   */
+  bool isConnected(Edge* e);
+  /*!
+   * \brief g this Occurrence occurs in the graph g
+   */
+  Graph* g;
+  /*!
+   * The container for edges
+   */
+  std::set<Edge*> edges;
+  /*!
+   * The container for vertices
+   */
+  std::set<Vertex*> vertices;
 };
 }
 
