7cc7b0642bb8ddc920ef9b88742164bc5f91bc52
Patrick Sittel <sittel@uni-kassel.de>
2018-04-25 14:42:05 +0200

    fixed init values of IPL scheduler base

--- src/HatScheT/base/ILPSchedulerBase.cpp
+++ src/HatScheT/base/ILPSchedulerBase.cpp
@@ -6,11 +6,11 @@ namespace HatScheT
 
 ILPSchedulerBase::ILPSchedulerBase(std::list<std::string> solverWishlist) : solver(new ScaLP::Solver(solverWishlist))
 {
-  this->threads = 0;
+  this->threads = 1;
   this->timeoutCounter = 0;
-  this->solverQuiet = false;
+  this->solverQuiet = true;
   this->optimalResult = false;
-  this->solverTimeout = 3600;
+  this->solverTimeout = 300;
   this->solvingTime = 0;
   this->totalTime = 0;
   this->scheduleFound = false;
