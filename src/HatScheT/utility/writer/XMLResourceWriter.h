e88c3128519ff0ac94b5fa298b230808cd28c708
Patrick Sittel <sittel@uni-kassel.de>
2018-10-12 12:12:14 +0200

    add xml resource write class

---
+++ src/HatScheT/utility/writer/XMLResourceWriter.h
@@ -0,0 +1,44 @@
+/*
+    This file is part of the HatScheT project, developed at University of Kassel and TU Darmstadt, Germany
+    Author: Patrick Sittel (sittel@uni-kassel.de)
+
+    Copyright (C) 2018
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#ifdef USE_XERCESC
+#pragma once
+
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/framework/StdOutFormatTarget.hpp>
+#include <xercesc/framework/LocalFileFormatTarget.hpp>
+
+#include "Writer.h"
+#include "../../ResourceModel.h"
+#include "../../Graph.h"
+
+using namespace xercesc;
+
+namespace HatScheT {
+
+
+class XMLResourceWriter : public Writer {
+
+};
+
+}
+
+#endif
