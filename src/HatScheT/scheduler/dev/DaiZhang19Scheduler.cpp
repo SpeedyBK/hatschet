29bba3235d0c2ed78279fe86bc117d7a84ff02de
Benjamin Kessler <bkessler@animal.digi.e-technik.uni-kassel.de>
2019-09-16 22:55:12 +0200

    Changed the SCC-Class to prevent to many copies for verticies. Adapted the Kosajaru Algorithm to these changes and implemented a new test for the Kosaraju Algorithm.

--- src/HatScheT/scheduler/dev/DaiZhang19Scheduler.cpp
+++ src/HatScheT/scheduler/dev/DaiZhang19Scheduler.cpp
@@ -11,7 +11,8 @@
 
 namespace HatScheT {
 
-  DaiZhang19Scheduler::DaiZhang19Scheduler(Graph &g, ResourceModel &resourceModel, std::list<std::string> solverWishlist)
+  DaiZhang19Scheduler::DaiZhang19Scheduler(Graph &g, ResourceModel &resourceModel,
+                                           std::list<std::string> solverWishlist)
       : SchedulerBase(g, resourceModel), ILPSchedulerBase(solverWishlist) {
     II = -1;
     this->timeouts = 0;
@@ -27,15 +28,45 @@ namespace HatScheT {
   void DaiZhang19Scheduler::schedule() {
     cout << endl << "DaiZhang19Scheduler::schedule: start" << endl;
 
+    cout << "-----------------------------------------------------------------------------------------------" << endl;
+    cout << "Finding the SCCs in Graph..." << endl;
+    cout << "-----------------------------------------------------------------------------------------------" << endl;
+    KosarajuSCC kosaraju(this -> g);
+    sccs = kosaraju.getSCCs();
 
+    cout << endl;
+    cout << "-----------------------------------------------------------------------------------------------" << endl;
+    cout << "Determine type of SCCs..." << endl;
+    cout << "-----------------------------------------------------------------------------------------------" << endl;
+    for (auto &it : sccs){
+      it->setSCCType(determineType(it));
+      cout << it->getName() << "Type is: " << it->getSccType() << " (0 = unknown, 1 = Basic, 2 = Complex, 3 = Trivial)" << endl;
+    }
+
+    cout << endl;
+    cout << "-----------------------------------------------------------------------------------------------" << endl;
+    cout << "Sorting the SCCs by type..." << endl;
+    cout << "-----------------------------------------------------------------------------------------------" << endl;
+    for (auto &it : sccs){
+      sortSCCs(it);
+    }
 
     cout << endl << "DaiZhang19Scheduler::schedule: done!" << endl;
-
   }
 
   scctype DaiZhang19Scheduler::determineType(SCC *scc) {
 
+    if (scc->getNumberOfVertices() == 1) {
+      return trivial;
+    }
 
+    list<Vertex*> verticiesOfSCC = scc->getVerticiesOfSCC();
+
+    for (auto &it:verticiesOfSCC) {
+      if (resourceModel.getResource(it)->getLimit() != -1) {
+        return complex;
+      }
+    }
 
     return basic;
   }
@@ -43,7 +74,18 @@ namespace HatScheT {
 
   int DaiZhang19Scheduler::getSccIdbyVertex(Vertex *v) {
 
+    int sccID = 0;
 
+    /*for (auto it:sccs) {
+      auto vertexMap = it->getVertexMap();
+      for (auto V:it->Vertices()) {
+        if (vertexMap[V] == v) {
+          sccID = it->getId();
+        }
+      }
+    }*/
+
+    return sccID;
 
   }
 
@@ -108,10 +150,5 @@ namespace HatScheT {
     }
   }
 
-  std::pair<Graph*, map<Vertex*, Vertex*>> DaiZhang19Scheduler::buildSupergraph(vector <SCC*> superGraph) {
-
-  }
 
 }
-
-
