b45506ed5f7a3df6e15de7f5ac0e0a1cd9577f1c
Benjamin Kessler <bkessler@animal.digi.e-technik.uni-kassel.de>
2019-12-19 22:48:35 +0100

    Implementing of a Fibonacci Heap to implement fast priority queues. Should speed up Dijkstras Algorithm to solve SDC Systems faster. Needed for SDS- and DaiZhang19 Scheduler. But probably useful for other stuff as well.

---
+++ src/HatScheT/utility/FibonacciHeap.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bkessler on 18/12/19.
+//
+
+#include "FibonacciHeap.h"
---
+++ src/HatScheT/utility/FibonacciHeap.h
@@ -0,0 +1,14 @@
+//
+// Created by bkessler on 18/12/19.
+//
+
+#ifndef HATSCHET_FIBONACCIHEAP_H
+#define HATSCHET_FIBONACCIHEAP_H
+
+
+class FibonacciHeap {
+
+};
+
+
+#endif //HATSCHET_FIBONACCIHEAP_H
